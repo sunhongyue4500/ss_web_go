b18cb521197b80147627370c1853cb106f0086b3
Hongyue Sun <sunhongyue4500@gmail.com>
2020-02-11 15:37:34 +0800

    U udpate build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 # kill myruirui 容器
-docker kill myruirui
+DOCKER_IMAGE="myruirui:v0.1"
+DOCKER_CONTAINER="myruirui"
+REPO="https://github.com/sunhongyue4500/ss_web_go.git"
+
+# kill
+str=`docker ps | grep $DOCKER_CONTAINER | wc -l`
+# echo $str
+if [ $str -eq 1 ]; then
+        docker kill $DOCKER_CONTAINER
+fi
+
 # rm myruirui:v0.1 镜像
-docker image rm myruirui:v0.1
+str=`docker image ls | grep $DOCKER_IMAGE | wc -l`
+# echo $str
+if [ $str -eq 1 ]; then
+        docker image rm $DOCKER_IMAGE
+fi
+
 # 从github上拉最新代码，
-git pull https://github.com/sunhongyue4500/ss_web_go.git
+git pull $REPO
 # build 镜像
-docker build -t myruirui:v0.1 .
+docker build -t $DOCKER_IMAGE .
 # run 容器
-docker run --name myruirui  -p 7777:7777 --rm myruirui:v0.1 -d
+docker run --name $DOCKER_CONTAINER  -p 7777:7777 --rm $DOCKER_IMAGE -d
 
 
 
